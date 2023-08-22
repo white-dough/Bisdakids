be7216f1605830a7e28b0b927af6447593380c70
white-dough <dwight.eyac20@gmail.com>
2023-08-22 20:22:44 +0800

    added first animation, tweening

--- scenes/levels/ObjectAnimation.gd
+++ scenes/levels/ObjectAnimation.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 
-#func animate_object():
-#	var object_found_tween: Tween = create_tween().set_parallel(true)
-##	object_found_tween.tween_property()
+func animate_object():
+	var object_found_tween: Tween = create_tween()
+	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(2,2),1)
+	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(1,1),1)
+	
 
 func _on_objects_object_found(object_name: Area2D):
 	print(object_name)
-	var object_animation = object_name.get_child(1).duplicate()
-	$BaseObject.add_child(object_animation)
+	var object_animation = object_name.duplicate()
+	self.add_child(object_animation)
+	animate_object()
