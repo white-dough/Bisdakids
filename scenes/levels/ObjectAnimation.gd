8cb25a1cc33c536663bc689c3a6267974d116d32
codynicx <alcovergero@gmail.com>
2023-09-06 15:44:34 +0800

    some changes

--- scenes/levels/ObjectAnimation.gd
+++ scenes/levels/ObjectAnimation.gd
@@ -1,26 +1,29 @@
 extends Node2D
 
-#delete object
-#animate scale modako, nya add particles, position to HUD text label
-
-#func _after_animate_remove(object_name):
-	#object_name.queue_free()
-
-func animate_object():
+func animate_object_found(object_found):
 	var object_found_tween: Tween = create_tween()
 	# basically these 5 'tweens' will play in sequence (sunod2, murag frame if nag animate)
-	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(1.1,1.1), 0.2) # has 4 parameters (object, property, final_val, duration)
-	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(1.2,1.2), 0.2)
-	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(1.3,1.3), 0.2)
-	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(0.6,0.6), 0.2) # shrinks the clicked object
-	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(0.5,0.5), 0.2) # also shrinks the object
-	object_found_tween.tween_property(self.get_child(0), "position", Vector2(500, 500), 1) # this line moves the clicked object down
-	object_found_tween.finished.connect(self.get_child(0).queue_free) # this line of code removes the object 
+#	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(1.1,1.1), 0.2) # has 4 parameters (object, property, final_val, duration)
+#	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(1.2,1.2), 0.2)
+#	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(1.3,1.3), 0.2)
+#	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(0.6,0.6), 0.2) # shrinks the clicked object
+#	object_found_tween.tween_property(self.get_child(0), "scale", Vector2(0.5,0.5), 0.2) # also shrinks the object
+	object_found_tween.tween_property(object_found, "position", Vector2(500, 500), 3) # this line moves the clicked object down
+	object_found_tween.finished.connect(object_found.queue_free) # this line of code removes the object 
 
+func animate_object_hint(object_hint):
+	var object_hint_tween: Tween = create_tween()
+#	object_hint_tween.tween_property()
 
-func _on_objects_object_found_signal(object_name):
-	print(object_name) # prints the object being clicked
-	var object_animation = object_name.duplicate()
-	add_child(object_animation)
+func _on_game_scene_object_found_signal(object_name):
+	var object_to_animate = object_name.duplicate()
+	for node in object_to_animate.get_children():
+		if node.is_class("CollisionPolygon2D"):
+			node.free()
+	add_child(object_to_animate)
 	object_name.queue_free() # remove 
-	animate_object() # calls the function above
+	animate_object_found(object_to_animate) # calls the function above
+
+func _on_hud_clue_pressed(object_clue):
+	var object_found_tween: Tween = create_tween()
+	object_found_tween.tween_property(object_clue, "scale", Vector2(1.2, 1.2), 2)
--- scenes/levels/hud.gd
+++ scenes/levels/hud.gd
@@ -1,12 +1,14 @@
 extends Control
-#@onready var object_list_container = $ColorRect/Panel/ContainerHUD/Objectlist
+signal clue_pressed(object_clue)
+@onready var object_list_container: VBoxContainer = $ColorRect/Panel/ContainerHUD/Objectlist
+@onready var history_current_objects: Array = []
 
-func object_list_label(current_objects):
-	var object_list_container = $ColorRect/Panel/ContainerHUD/Objectlist
-#	print(object_list_container)
-	
+func object_list_label(current_objects_strings: Array, history_current_objects: Array):
 	for i in range(object_list_container.get_child_count()):
-		var substring = current_objects[i].to_string().split(":",true,1)
-		var label = substring[0]
-		object_list_container.get_child(i).set_text(label)
+		var current_label = object_list_container.get_child(i)
+		current_label.set_text(current_objects_strings[i])
+	self.history_current_objects = history_current_objects
 
+func _on_clue_pressed():
+	if not history_current_objects.is_empty():
+		clue_pressed.emit(history_current_objects[0])
