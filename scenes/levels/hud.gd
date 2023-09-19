f2ce32ac8ecda73037a8dcdf5d322680288743c8
white-dough <dwight.eyac20@gmail.com>
2023-09-20 01:38:40 +0800

    game autoload singleton

--- scenes/levels/hud.gd
+++ scenes/levels/hud.gd
@@ -18,14 +18,18 @@ func _on_level_1_ready():
 	timer.set_wait_time(level_time)
 	timer.start()
 	pause_timer.timeout.connect(time_freeze)
+	
 
 func _process(_delta):
 	progress_bar.value = timer.get_time_left()
+#	print(current_objects_strings)
 	
 func object_list_label(current_objects_strings: Array):
 	for i in range(object_list_container.get_child_count()):
 		var current_label = object_list_container.get_child(i)
 		current_label.set_text(current_objects_strings[i])
+		
+	
 
 func _on_clue_pressed():
 	if not history_current_objects.is_empty():
@@ -39,7 +43,7 @@ func _on_timer_timeout():
 func _on_time_freeze_pressed():
 	if pause_timer.is_stopped():
 		pause_timer.start()
-		time_freeze()		
+		time_freeze()
 
 func time_freeze():
 	if timer.is_paused():
