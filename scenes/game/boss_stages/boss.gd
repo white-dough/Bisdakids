113892954968f0ea1b9a2e6ba9056e7dfd4f08e3
white-dough <dwight.eyac20@gmail.com>
2023-11-09 09:56:00 +0800

    boss level complete logic

--- scenes/game/boss_stages/boss.gd
+++ scenes/game/boss_stages/boss.gd
@@ -20,17 +20,23 @@ extends Node2D
 @onready var highscore_label : Label = level_success.find_child('HighscoreLbl')
 @onready var score_label : Label = level_success.find_child('ScoreLbl')
 
+@onready var wave_node : TextureProgressBar = $WaveProg/WaveProg
+@onready var current_wave : int = int(wave_node.get_value())
+
+@onready var background_node : CanvasLayer = $GameScene/Background2
+@onready var wave2_node : CanvasLayer = $GameScene/BossTigmu12
+@onready var wave3_node : CanvasLayer = $GameScene/BossIdentification13
 
 func _ready():
 	Audio.play_bgm(Audio.boss_bgm)
 	$HUD.connect("level_finished", level_completed)
-	$HUD.connect("wave_finished", wave_finished)
 	success_prompt.visibility_changed.connect(load_interstitial_ad)
 	failed_prompt.visibility_changed.connect(load_interstitial_ad)
 	BannerAds.destroy_banner()
 	
 func load_interstitial_ad():
-	InterstitialAds.load_show_interstitial()
+	if not Game.premium:
+		InterstitialAds.load_show_interstitial()
 
 func _process(_delta):
 	pass
@@ -42,29 +48,46 @@ func _process(_delta):
 #		if not object is Area2D:
 #			print(object)
 
-func wave_finished(wave: int) -> void:
-	pass
-
+func wave_finished() -> void:
+	current_wave += 1
+	wave_node.set_value(current_wave)	
+	background_node.show()
+	match(current_wave):
+		#show the modal, set the flag
+		2:
+			wave2_node.show()
+		3:
+			wave2_node.hide()
+			wave3_node.show()
+		4:
+			background_node.hide()
+			wave3_node.hide()
+			var time_left : int = int($HUD/ColorRect/Panel/ContainerHUD/TimerBar/Timer.time_left)
+			var prompt_timer : Timer = Timer.new()
+			prompt_timer.wait_time = 3
+			add_child(prompt_timer)
+			success_prompt.visible = true
+			prompt_timer.start()
+			await prompt_timer.timeout
+			var score: int = 5000 * (time_left / level_time)
+			if Game.progress[level_name] < score:
+				highscore_label.set_text(str(int(score)))#new highscore
+				Game.progress[level_name] = int(score)
+				Game.progress["timestamp"] = Time.get_unix_time_from_system()
+				Game.update_local_save()
+			else:
+				highscore_label.set_text(str(Game.progress[level_name]))
+			score_label.set_text(str(int(score)))
+			star_bar.set_value(score)
+			level_success.visible = true
+		
 func level_completed(time_left):
 	if time_left > 0:
-		var prompt_timer : Timer = Timer.new()
-		prompt_timer.wait_time = 3
-		add_child(prompt_timer)
-		success_prompt.visible = true
-		prompt_timer.start()
-		await prompt_timer.timeout
-		var score: int = 5000 * (time_left / level_time)
-		if Game.progress[level_name] < score:
-			highscore_label.set_text(str(int(score)))#new highscore
-			Game.progress[level_name] = int(score)
-			Game.progress["timestamp"] = Time.get_unix_time_from_system()
-			Game.update_local_save()
-		else:
-			highscore_label.set_text(str(Game.progress[level_name]))
-		score_label.set_text(str(int(score)))
-		star_bar.set_value(score)
-		level_success.visible = true
+		wave_finished()
 	else:
+		background_node.hide()
+		wave3_node.hide()
+		wave2_node.hide()
 		var prompt_timer : Timer = Timer.new()
 		prompt_timer.wait_time = 3
 		add_child(prompt_timer)
