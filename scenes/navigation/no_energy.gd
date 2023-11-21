9acaf5ab5a44d6912c28e7c6b398f076d2348d18
codynicx <alcovergero@gmail.com>
2023-11-21 11:07:18 +0800

    modal for insufficient coins, no energy, error modal, & more

---
+++ scenes/navigation/no_energy.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_close_btn_pressed():
+	Audio.play_sfx(Audio.close_btn_sfx)
+	queue_free()
+
+func _on_dili_btn_pressed():
+	Audio.play_sfx(Audio.normal_btn_sfx)
+	queue_free()
+
+func _on_buy_btn_pressed():
+	Audio.play_sfx(Audio.normal_btn_sfx)
+	get_tree().change_scene_to_file("res://scenes/navigation/store_inventory/store/store.tscn")
